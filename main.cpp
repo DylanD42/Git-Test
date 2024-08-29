f92f2629e3cc7902a3bce800498039227af0bf70
Dylan Daniels <ddanie31@ut.utm.edu>
2024-08-29 16:49:21 -0500

    updated main.cpp!

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main() {
-  cout << "Hello World!" << endl;
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  cout << endl << sum(n);
   
   return 0;
+}
+
+int sum(int n){
+  int total = 0;
+  for(int i = 1; i <= n; i++){
+    total += i;
+  };
+  return total;
 }
